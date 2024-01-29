931bc25c06603026d2b4a2676135a14b15300079
Doridian <git@doridian.net>
2024-01-28 21:14:00 -0800

    Implement actual reading start and getting readings in

--- __main__.py
+++ __main__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from netdaq import NetDAQ
+from netdaq import NetDAQ, DAQConfigBits, DAQConfiguration, DAQMeasuremenType, DAQRange, DAQChannelConfiguration
 from sys import argv
+from time import sleep
 
 instrument = NetDAQ(argv[1], 4369)
 instrument.connect()
@@ -12,19 +13,43 @@ print("Version info", instrument.get_version_info())
 
 instrument.wait_for_idle()
 instrument.stop()
+instrument.set_monitor_channel(0)
 
 print("LC version", instrument.get_lc_version())
 
 instrument.set_time()
 print("Time set!")
 
-# Configure channels
+instrument.set_config(DAQConfiguration(
+    bits=DAQConfigBits.INTERVAL_TRIGGER.value | DAQConfigBits.DRIFT_CORRECTION.value | DAQConfigBits.TOTALIZER_DEBOUNCE.value,
+    interval_time=1.0,
+    alarm_time=1.0,
+    phy_channels=[
+        DAQChannelConfiguration(
+            mtype=DAQMeasuremenType.VDC,
+            range=DAQRange.VDC_3V,
+        ),
+        DAQChannelConfiguration(
+            mtype=DAQMeasuremenType.VDC,
+            range=DAQRange.VDC_3V,
+        ),
+    ],
+    computed_channels=[],
+))
+print("Config set!")
 
-# 0x71
-# 0x67
-# 0x64 .....
+instrument.reset_totalizer()
+instrument.start()
+instrument.set_monitor_channel(1)
 
-# 0x76
-# 0x68
+try:
+    while True:
+        print("Readings", instrument.get_readings())
+        sleep(1)
+except KeyboardInterrupt:
+    pass
+
+instrument.set_monitor_channel(0)
+instrument.stop()
 
 instrument.close()
--- netdaq.py
+++ netdaq.py
@@ -1,25 +1,89 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from datetime import datetime
+from enum import Enum
+from dataclasses import dataclass
+from struct import pack, unpack
+from time import sleep
 
 class ResponseErrorCodeException(Exception):
     def __init__(self, code: int) -> None:
         super().__init__(f'Response error: {code:08x}')
         self.code = code
 
-class NetDAQ:
-    COMMAND_PING             = 0x00000000
-    COMMAND_CLOSE            = 0x00000001
-    COMMAND_STATUS_QUERY     = 0x00000002
-    COMMAND_STOP             = 0x00000068
-    COMMAND_SET_TIME         = 0x0000006A
-    COMMAND_GET_VERSION_INFO = 0x00000072
-    COMMAND_GET_BASE_CHANNEL = 0x00000077
-    COMMAND_GET_LC_VERSION   = 0x0000007F
-    COMMAND_SET_CHANNELS     = 0x00000081
+class NetDAQCommand(Enum):
+    PING                  = 0x00000000
+    CLOSE                 = 0x00000001
+    STATUS_QUERY          = 0x00000002
+    GET_READINGS          = 0x00000064
+    START                 = 0x00000067
+    STOP                  = 0x00000068
+    SET_TIME              = 0x0000006A
+    RESET_TOTALIZER       = 0x00000071
+    GET_VERSION_INFO      = 0x00000072
+    SET_MONITOR_CHANNEL   = 0x00000075
+    CLEAR_MONITOR_CHANNEL = 0x00000076
+    GET_BASE_CHANNEL      = 0x00000077
+    GET_LC_VERSION        = 0x0000007F
+    SET_CONFIG            = 0x00000081
 
+class DAQConfigBits(Enum):
+    MEDIUM_SPEED       = 0x0001
+    FAST_SPEED         = 0x0002
+    FAHRENHEIT         = 0x0004
+    TRIGGER_OUT        = 0x0008
+    DRIFT_CORRECTION   = 0x0010
+    TOTALIZER_DEBOUNCE = 0x0020
+    INTERVAL_TRIGGER   = 0x0040
+    ALARM_TRIGGER      = 0x0080
+    EXTERNAL_TRIGGER   = 0x0100
+
+class DAQMeasuremenType(Enum):
+    OFF = 0x00
+    Ohm = 0x01
+    VDC = 0x02
+
+class DAQRange(Enum):
+    RANGE_NONE = 0x00
+    VDC_90mV   = 0x2001
+    VDC_3V     = 0x2308
+
+@dataclass(frozen=True)
+class DAQChannelConfiguration:
+    mtype: DAQMeasuremenType = DAQMeasuremenType.OFF
+    range: DAQRange = DAQRange.RANGE_NONE
+    aux2: float = 0.0 # RTD ALpha
+    aux1: float = 0.0 # RTD R0 / Shunt resistance
+    extra_bits: int = 0
+    alarm_bits: int = 0
+    alarm1_level: float = 0.0
+    alarm2_level: float = 0.0
+    alarm1_digital: int = 0
+    alarm2_digital: int = 0
+    mxab_multuplier: float = 1.0
+    mxab_offset: float = 0.0
+
+@dataclass
+class DAQConfiguration:
+    bits: int
+    interval_time: float
+    alarm_time: float
+    phy_channels: list[DAQChannelConfiguration]
+    computed_channels: list[DAQChannelConfiguration]
+
+@dataclass(frozen=True)
+class DAQReading:
+    time: datetime
+    null1: int
+    null2: int
+    null3: int
+    values: list[float]
+
+class NetDAQ:
     _FIXED_HEADER = bytearray([0x46, 0x45, 0x4C, 0x58])
     _HEADER_LEN = 16
     _INT_LEN = 4
+    _CHANNEL_COUNT_PHY = 20
+    _CHANNEL_COUNT_COMPUTED = 10
 
     def __init__(self, ip: str, port: int) -> None:
         self.ip = ip
@@ -37,7 +101,7 @@ class NetDAQ:
             pass
 
         try:
-            self.send_rpc(self.COMMAND_CLOSE)
+            self.send_rpc(NetDAQCommand.CLOSE)
         except:
             pass
 
@@ -57,18 +121,41 @@ class NetDAQ:
         return int.to_bytes(int(value), self._INT_LEN, 'big')
 
     def _parse_float(self, data: bytes) -> float:
-        return 0 # TODO: float.from_bytes(data, 'big')
+        return unpack('>f', data[:4])
 
     def _make_float(self, value: float) -> None:
-        return [0,0,0,0] # TODO: implement
+        return pack('>f', value)
 
-    def send_rpc(self, command: int, payload: bytes = b'') -> bytes:
+    def _parse_time(self, data: bytes) -> datetime:
+        return datetime(
+            hour=data[0],
+            minute=data[1],
+            second=data[2],
+            month=data[3],
+            day=data[5],
+            year=2000 + data[6], # Well this is gonna break in 2100 lol
+            microsecond=(self._parse_int(data[8:]) & 0xFFFF) * 1000,
+        )
+
+    def _make_time(self, time: datetime) -> bytes:
+        return bytes([
+            time.hour,
+            time.minute,
+            time.second,
+            time.month,
+            0x08, # unknown value
+            time.day,
+            time.year % 100,
+            0x00, # unknown value
+        ])
+
+    def send_rpc(self, command: NetDAQCommand, payload: bytes = b'') -> bytes:
         sequence_id = self.sequence_id
         self.sequence_id += 1
 
         packet = self._FIXED_HEADER + \
                     self._make_int(sequence_id) + \
-                    self._make_int(command) + \
+                    self._make_int(command.value) + \
                     self._make_int(len(payload) + self._HEADER_LEN) + \
                     payload
 
@@ -97,14 +184,15 @@ class NetDAQ:
         return payload
 
     def ping(self) -> None:
-        self.send_rpc(self.COMMAND_PING)
+        self.send_rpc(NetDAQCommand.PING)
+
+    def reset_totalizer(self) -> None:
+        self.send_rpc(NetDAQCommand.RESET_TOTALIZER)
 
     def get_base_channel(self) -> int:
-        return self._parse_int(self.send_rpc(self.COMMAND_GET_BASE_CHANNEL))
+        return self._parse_int(self.send_rpc(NetDAQCommand.GET_BASE_CHANNEL))
 
-    def get_version_info(self, command: int = 0) -> list[str]:
-        if not command:
-            command = self.COMMAND_GET_VERSION_INFO
+    def get_version_info(self, command: NetDAQCommand = NetDAQCommand.GET_VERSION_INFO) -> list[str]:
         data = self.send_rpc(command)
         blobs = []
         current = []
@@ -119,41 +207,117 @@ class NetDAQ:
         return blobs
 
     def get_lc_version(self) -> str:
-        return self.get_version_info(command=self.COMMAND_GET_LC_VERSION)
+        return self.get_version_info(command=NetDAQCommand.GET_LC_VERSION)
 
     def wait_for_idle(self) -> None:
         while True:
-            status = self._parse_int(self.send_rpc(self.COMMAND_STATUS_QUERY))
-            if status == 0x00000000:
+            status = self._parse_int(self.send_rpc(NetDAQCommand.STATUS_QUERY))
+            if status & 0x80000000 == 0x00000000:
                 break
+            sleep(0.01)
 
     def set_time(self, time: datetime | None = None) -> None:
         if not time:
             time = datetime.now()
 
-        packet = bytes([
-            time.hour,
-            time.minute,
-            time.second,
-            time.month,
-            0x08, # random value
-            time.day,
-            time.year % 100,
-            0x00, # random value
-        ]) + self._make_int(time.microsecond / 1000)
+        packet = self._make_time(time) + self._make_int(time.microsecond / 1000)
 
-        self.send_rpc(self.COMMAND_SET_TIME, packet)
+        self.send_rpc(NetDAQCommand.SET_TIME, packet)
         self.wait_for_idle()
 
-    def set_channels(self, config) -> None:
-        # TODO: All of this
+    def set_config(self, config: DAQConfiguration) -> None:
+        payload = self._make_int(config.bits) + \
+                    bytes([0x00, 0x00, 0x00, 0x00]) + \
+                    bytes([0x00, 0x00, 0x00, 0x00]) + \
+                    self._make_int(config.interval_time) + \
+                    self._make_int(int(config.interval_time * 1000) % 1000) + \
+                    bytes([0x00, 0x00, 0x00, 0x00]) + \
+                    bytes([0x00, 0x00, 0x00, 0x00]) + \
+                    self._make_int(config.alarm_time) + \
+                    self._make_int(int(config.alarm_time * 1000) % 1000) + \
+                    bytes([0x00, 0x00, 0x00, 0x00]) + \
+                    bytes([0x00, 0x00, 0x00, 0x00]) + \
+                    bytes([0x00, 0x00, 0x00, 0x00]) + \
+                    bytes([0x00, 0x00, 0x00, 0x64])
+
+        phy_channels = config.phy_channels
+        if len(phy_channels) < self._CHANNEL_COUNT_PHY:
+            phy_channels += [DAQChannelConfiguration() for _ in range(self._CHANNEL_COUNT_PHY - len(phy_channels))]
+
+        for chan in phy_channels:
+            payload += self._make_int(chan.mtype.value) + \
+                        self._make_int(chan.range.value) + \
+                        self._make_float(chan.aux2) + \
+                        self._make_float(chan.aux1) + \
+                        self._make_int(chan.extra_bits) + \
+                        self._make_int(chan.alarm_bits) + \
+                        self._make_float(chan.alarm1_level) + \
+                        self._make_float(chan.alarm2_level) + \
+                        self._make_int(chan.alarm1_digital) + \
+                        self._make_int(chan.alarm2_digital) + \
+                        self._make_float(chan.mxab_multuplier) + \
+                        self._make_float(chan.mxab_offset)
+
+        computed_channels = config.computed_channels
+        if len(computed_channels) < self._CHANNEL_COUNT_COMPUTED:
+            computed_channels += [DAQChannelConfiguration() for _ in range(self._CHANNEL_COUNT_COMPUTED - len(computed_channels))]
+
+        for chan in computed_channels:
+            payload += self._make_int(chan.mtype.value) + \
+                        self._make_int(chan.range.value) + \
+                        self._make_float(chan.aux2) + \
+                        self._make_float(chan.aux1) + \
+                        self._make_int(chan.extra_bits) + \
+                        self._make_int(chan.alarm_bits) + \
+                        self._make_float(chan.alarm1_level) + \
+                        self._make_float(chan.alarm2_level) + \
+                        self._make_int(chan.alarm1_digital) + \
+                        self._make_int(chan.alarm2_digital) + \
+                        self._make_float(chan.mxab_multuplier) + \
+                        self._make_float(chan.mxab_offset)
+
+        payload = payload + bytes([0x00] * (2492 - len(payload)))
+        self.send_rpc(NetDAQCommand.SET_CONFIG, payload)
         self.wait_for_idle()
+
+    def set_monitor_channel(self, channel: int) -> None:
+        if channel <= 0:
+            self.send_rpc(NetDAQCommand.CLEAR_MONITOR_CHANNEL)
+        else:
+            self.send_rpc(NetDAQCommand.SET_MONITOR_CHANNEL, self._make_int(channel))
+
+    def get_readings(self) -> list[DAQReading]:
+        data = self.send_rpc(NetDAQCommand.GET_READINGS, b'\x00\x00\x00\xFF')
+        result: list[DAQReading] = []
+
+        chunk_length = self._parse_int(data[0:])
+        chunk_count = self._parse_int(data[4:])
+        # 8:12 = unknown/null
+
+        chunks_buf = data[12:]
+        for i in range(chunk_count):
+            chunk_data = chunks_buf[i * chunk_length:(i + 1) * chunk_length]
+            if self._parse_int(chunk_data[0:]) != 0x10:
+                raise Exception('Invalid chunk header')
+
+            result.append(DAQReading(
+                time=self._parse_time(chunk_data[4:]),
+                null1=self._parse_int(chunk_data[16:]),
+                null2=self._parse_int(chunk_data[20:]),
+                null3=self._parse_int(chunk_data[24:]),
+                values=[self._parse_float(chunk_data[i:]) for i in range(28, len(chunk_data), 4)],
+            ))
+
+        return result
 
     def stop(self) -> None:
         try:
-            self.send_rpc(self.COMMAND_STOP)
+            self.send_rpc(NetDAQCommand.STOP)
         except ResponseErrorCodeException:
             pass
+
+    def start(self) -> None:
+        self.send_rpc(NetDAQCommand.START, b'\x00' * 16)
 
     def handshake(self) -> None:
         self.ping()
